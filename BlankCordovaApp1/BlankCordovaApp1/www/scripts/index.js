cdde6872679a844f7905238ba38a6ab406dc9e27
marijatrajkovic <marija94trajkovic@live.com>
2017-05-02 21:26:50 +0200

    copy

---
+++ BlankCordovaApp1/BlankCordovaApp1/www/scripts/index.js
@@ -0,0 +1,90 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener('resume', onResume.bind(this), false);
+        document.getElementById("btnTakePhoto").onclick = function () {
+            navigator.camera.getPicture(function (imageUri) {
+                   navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+            }, null, null)
+        };
+
+
+        var element = document.getElementById("deviceready");
+        element.innerHTML = 'Device Ready';
+        element.className += ' ready';
+     
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+     
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+
+   
+})();
+
+document.getElementById('login').onclick = function () {
+    location.href = "home.html";
+};
+    
+
+function onPhotoDataSuccess(imageData) {
+    // Get image handle
+    var smallImage = document.getElementById('smallImage');
+
+    // Unhide image elements
+    smallImage.style.display = 'block';
+    smallImage.src = imageData;
+}
+
+//Callback function when the picture has not been successfully taken
+function onFail(message) {
+    alert('Failed to load picture because: ' + message);
+}
+
+function movePic(file) {
+    window.resolveLocalFileSystemURI(file, resolveOnSuccess, resOnError);
+}
+
+//Callback function when the file system uri has been resolved
+function resolveOnSuccess(entry) {
+    var d = new Date();
+    var n = d.getTime();
+    //new file name
+    var newFileName = n + ".jpg";
+    var myFolderApp = "EasyPacking";
+
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSys) {
+        //The folder is created if doesn't exist
+        fileSys.root.getDirectory(myFolderApp,
+                        { create: true, exclusive: false },
+                        function (directory) {
+                            entry.moveTo(directory, newFileName, successMove, resOnError);
+                        },
+                        resOnError);
+    },
+    resOnError);
+}
+
+//Callback function when the file has been moved successfully - inserting the complete path
+function successMove(entry) {
+    //I do my insert with "entry.fullPath" as for the path
+}
+
+function resOnError(error) {
+    alert(error.code);
+}
